00d1b9e2b751f26c4b8cdcefaa50be11e1f0d97e
saabdulla <nonemask1@gmail.com>
2026-01-19 00:07:21 +0300

    introducation to classes and pointers in js

---
+++ 1.classesAndPointers/classes.js
@@ -0,0 +1,25 @@
+class Cookie {
+    constructor(color) {
+        this.color = color
+    }
+
+    getColor(){
+        return this.color
+    }
+
+    setColor(color) {
+        this.color = color
+    }
+
+}
+
+let cookieOne = new Cookie('green')
+let cookieTwo = new Cookie('blue')
+
+console.log(cookieOne.getColor())
+cookieOne.setColor("yellow")
+console.log(cookieOne.getColor())
+console.log(cookieTwo.getColor())
+
+
+
---
+++ 1.classesAndPointers/pointers.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2 = num1;
+
+let obj1 = {val:11}
+let obj2 = obj1
+
+console.log(obj2)
+
+let obj3 = {value: 57}
+
+obj2 = obj3
+obj1 = obj2
+
