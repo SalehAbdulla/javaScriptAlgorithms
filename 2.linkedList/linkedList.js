b481e0439b6d6b87b61f9dd4e2c1c0aac3334af7
saabdulla <nonemask1@gmail.com>
2026-01-28 13:21:50 +0300

    print() method and unshift() method

--- 2.linkedList/linkedList.js
+++ 2.linkedList/linkedList.js
@@ -47,15 +47,52 @@ class LinkedList {
             this.tail = null
         }
         return temp
+    }   
+    
+
+
+    unshift(value){
+        const newValue = new Node(value);
+        if (!this.head) {
+            this.head = newValue;
+            this.tail = newValue;
+        } else {
+            newValue.next = this.head
+            this.head = newValue
+        }
+
+        this.length++
+        return this;
+    }
+
+    print(){
+        let newLength = this.length;
+        let buffer = "";
+        let newHead = this.head;
+
+        while (newLength != 0) {
+            if (newHead) {
+                buffer += newHead.value + " -> "
+                newHead = newHead.next;
+            }
+            newLength--
+        }
+        console.log(buffer)
     }
 
 }
 
 
-let myLinkedList = new LinkedList(7)
-myLinkedList.push(4)
+let myLinkedList = new LinkedList(0)
 
-console.log(myLinkedList)
-console.log(myLinkedList.pop())
-console.log(myLinkedList)
+myLinkedList.push(1)
+myLinkedList.push(2)
+myLinkedList.push(3)
+myLinkedList.push(4)
+myLinkedList.push(5)
+
+
+myLinkedList.unshift(-1);
+
+myLinkedList.print();
 
